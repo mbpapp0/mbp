8af311a24fea4f8410bc3e2afa3ba835c3aff562
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 05:55:37 +0100

    Update DataAdminIEGReducedPrice.jsx

--- frontend/src/pages/DataAdminIEGReducedPrice.jsx
+++ frontend/src/pages/DataAdminIEGReducedPrice.jsx
@@ -36,7 +36,7 @@ export default function DataAdminIEGReducedPrice() {
         
     if(loading){
       return(
-       <div className='container flex justify align' style={{height: '100%}}>
+       <div className='container flex justify align' style={{height: '100%'}}>
          <h3>Loading</h3>
        </div>
       )
