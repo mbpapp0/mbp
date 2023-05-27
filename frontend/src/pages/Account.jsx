cc04aac286c6683d8a2c3762e1b1b952543cb523
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 12:11:11 +0100

    Update and rename DataAdminExplore.jsx to Account.jsx

---
+++ frontend/src/pages/Account.jsx
@@ -0,0 +1,7 @@
+export default function Account() {
+    return(
+        <div className='container'>
+            <h3>Account</h3>
+        </div>
+    )
+}
--- frontend/src/pages/DataAdminExplore.jsx
+++
@@ -1,7 +0,0 @@
-export default function DataAdminExplore() {
-    return(
-        <div>
-            
-        </div>
-    )
-}
