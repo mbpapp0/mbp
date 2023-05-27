725c00326b09c5ff0daa7a691996aef89f7206d6
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 16:38:59 +0100

    Update Account.jsx

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -4,7 +4,8 @@ export default function Account() {
     const { id } = useParams();
     const user = JSON.parse(localStorage.getItem('user'));
     const [info, setInfo] = useState(false);
-    const [editing, setEditing] = useState(false)
+    const [editing, setEditing] = useState(false);
+    const [error, setError] = useState(false);
     
     const getData = async () => {
         const response = await fetch(`https://mbp-server.onrender.com/api/users/singleuser/${id}`);
@@ -12,6 +13,68 @@ export default function Account() {
         setInfo(json);
 
     } 
+    
+    function isValidEmail(email) {
+        var pattern = /^[\w.-]+@[\w.-]+\.\w+$/;
+        return pattern.test(email);
+    }
+    
+    function isValidName(name) {
+        const invalidCharacters = /[.,\\\/#!$%\^&\*;:{}=\-_`~()0-9]/;
+        if(name[0] === " "){
+            return false; 
+        }
+        return !invalidCharacters.test(name);
+    }
+    
+    const editData = async() => {
+        
+        const isEmail = isValidEmail(email);
+        const isName = isValidName(name)
+        
+        
+        if(!isEmail){
+          setError('Please enter a valid email');
+            return;
+        }
+        
+    
+            if(!isName){
+            setError('Please enter a valid Name');
+            return;
+        }
+
+        if(!name || !email){
+            setError('Please fill in all fields');
+        }
+
+        const data = {
+            name,
+            email,
+        }
+
+        const response = await fetch(`https://mbp-server.onrender.com/api/users/edit/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // const response = await fetch(`http://localhost:3001/api/users/edit/${id}`, {
+        //     method: 'PUT',
+        //     body: JSON.stringify(data),
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // });
+
+        if(response.ok){
+            window.history.back();
+        }
+
+    }
+
     
     useEffect(() => {
        getData()
