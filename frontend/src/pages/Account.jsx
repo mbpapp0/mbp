4a66697e10f5d5ccb5d839a67d657f1b3246441a
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 15:13:50 +0100

    Update Account.jsx

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 export default function Account() {
+    const { id } = useParams();
+    const user = JSON.parse(localStorage.getItem('user'); bb
+    const [info, setInfo] = useState(false);
+    
     return(
         <div className='container'>
             <h3>Account</h3>
+             <p>{ user.role} { user.id }</p>
         </div>
     )
 }
