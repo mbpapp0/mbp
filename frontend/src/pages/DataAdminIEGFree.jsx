c20cea1ce71f8b5afbe267871d02161eb7eda964
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 05:49:03 +0100

    Update DataAdminIEGFree.jsx

--- frontend/src/pages/DataAdminIEGFree.jsx
+++ frontend/src/pages/DataAdminIEGFree.jsx
@@ -30,7 +30,7 @@ export default function DataAdminIEGFree() {
     
     if(loading){
       return(
-       <div className='container flex justify align'>
+       <div className='container flex justify align' style={{marginTop: '20rem'}}>
          <h3>Loading</h3>
        </div>
       )
