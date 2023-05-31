8099e302244968898f8a7f5f76c19e90396e4f47
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-31 08:03:56 +0100

    Update AdminUsers.jsx

--- frontend/src/pages/AdminUsers.jsx
+++ frontend/src/pages/AdminUsers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Map from '../components/Map';
 import Back from '../components/Back';
 import InfoCard from '../components/InfoCard';
+import { Link } from 'react-router-dom';
 
 function UserList() {
   const [users, setUsers] = useState([]);
@@ -29,16 +30,21 @@ function UserList() {
 
   return (
     <div className='container'>
-      <div className='info_hover'> 
-        <Map location='Admin Users Preview Screen'/>
-        <InfoCard description='This is the Admin Users Preview Screen, Where you can see users with System or Data Adminstrator roles'/>    
+      <div className='info_buttons>
+        <Back />
+        <div className='info_hover'> 
+          <Map location='Admin Users Preview Screen'/>
+          <InfoCard description='This is the Admin Users Preview Screen, Where you can see users with System or Data Adminstrator roles'/>    
+        </div>
        </div>
        <h2 className="block1x">Admin Users Lists</h2>
       <div>
         {users && users.map(user => (
           <div key={user._id} style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem', alignItems: 'center'}}>
             <p>{user.name}</p>
-            <button className="button radius">View User</button>
+            <div className='user_list_buttons'>
+               <Link to={`/users/${user._id}`}><button className='button radius user_list_button'>More Info</button></Link>
+             </div>
           </div>
         ))}
       </div>
