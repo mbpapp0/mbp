65a0f75c928faecfdd5d63f4c882b7883f76d341
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-31 13:00:14 +0100

    Create PasswordRules.jsx and added boilerplate

---
+++ frontend/src/components/PasswordRules.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PasswordRules = () => {
+  const styles = {
+    container: {
+      background: 'black',
+      color: 'white',
+      padding: '20px',
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      <h3>Strong Password Rules:</h3>
+      <ul>
+        <li>Password must be at least 8 characters long.</li>
+        <li>Password must contain at least one uppercase letter.</li>
+        <li>Password must contain at least one lowercase letter.</li>
+        <li>Password must contain at least one digit.</li>
+        <li>Password must contain at least one special character (e.g., @, #, $).</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PasswordRules;
