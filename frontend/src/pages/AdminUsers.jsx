45a46d04077f8236e04ff6ba742bf42206d5be3e
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-31 08:31:44 +0100

    Update AdminUsers.jsx

--- frontend/src/pages/AdminUsers.jsx
+++ frontend/src/pages/AdminUsers.jsx
@@ -30,11 +30,11 @@ function UserList() {
 
   return (
     <div className='container'>
-      <div className='info_buttons>
-        <Back />
-             <Map location='Admin Users Preview Screen'/>
-             <InfoCard description='This is the Admin Users Preview Screen, Where you can see users with System or Data Adminstrator roles'/>    
-      </div>
+     // <div className='info_buttons>
+    //    <Back />
+            // <Map location='Admin Users Preview Screen'/>
+           //  <InfoCard description='This is the Admin Users Preview Screen, Where you can see users with System or Data Adminstrator roles'/>    
+      //</div>
        <h2 className="block1x">Admin Users Lists</h2>
       <div>
         {users && users.map(user => (
