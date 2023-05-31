2430e64ee85d37f99c6eca1187de0cd05b353307
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-31 11:55:32 +0100

    Update ForgotPassword.jsx

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -3,32 +3,34 @@ import React, { useState } from 'react';
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
   const [resetSent, setResetSent] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     // Perform your password reset logic here, e.g., send an email to the user's email address
     // ...
 
     // Set the resetSent state to true to show the success message
     setResetSent(true);
+    setLoading(false);
   };
 
   return (
-    <div>
+    <div className='container'>
       {resetSent ? (
         <p>Password reset link has been sent to your email.</p>
       ) : (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className='login'>
           <label>
             Email:
             <input type="email" value={email} onChange={handleEmailChange} />
           </label>
-          <button type="submit">Reset Password</button>
+          <button type="submit" disabled={loading}>{loading ? 'Loading...' : 'Reset Password'}</button>
         </form>
       )}
     </div>
