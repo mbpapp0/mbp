0b05625ca6f9ccc40f2cf7e6950f92855e951203
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-03 14:32:38 +0100

    Update ClientForm.jsx

--- frontend/src/pages/ClientForm.jsx
+++ frontend/src/pages/ClientForm.jsx
@@ -92,7 +92,7 @@ export default function ClientForm() {
     const [HouseholdMemberSixSSPR, setHouseholdMemberSixSSPR] = useState('');
     const [HouseholdMemberSixOther, setHouseholdMemberSixOther] = useState('');
 
-    const [totalHouseHoldMembers, setTotalHouseHoldMembers] = useState('')
+    const [totalHouseHoldMembers, setTotalHouseHoldMembers] = useState(calculateHouseholdMembers())
     const [totalHouseHoldIncome, setTotalHouseHoldIncome] = useState(0);
 
     const [ssn, setSsn] = useState('');
@@ -203,7 +203,6 @@ export default function ClientForm() {
   if (HouseholdMemberSixName) {
     count++;
   }
-  setTotalHouseHoldMembers(count)
   return count;
 }
     
