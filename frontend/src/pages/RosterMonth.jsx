acd018e270473977e6fd048c4a25d7652f12c70e
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-05 14:26:45 +0100

    Update RosterMonth.jsx

--- frontend/src/pages/RosterMonth.jsx
+++ frontend/src/pages/RosterMonth.jsx
@@ -46,17 +46,22 @@ export default function RosterMonth() {
     }
 
     const keysToRemove = ['_id', 'branch', 'month', 'createdAt', 'updatedAt', '__v'];
+    
+    const removeDetails = () => {
+      const newArray = data.map(obj => {
+          const newObj = {...obj};
+          keysToRemove.forEach(key => delete newObj[key]
+     );
+          return newObj
+      })
+    }
 
-    {/* const printout = data.map(obj => {
-     const newObj = { ...obj };
-     keysToRemove.forEach(key => delete newObj[key]);
-     return newObj;
-    }); */}
+    const print = removeDetails();
     
 
     const submit = () => {
         var wb = XLSX.utils.book_new(),
-        ws = XLSX.utils.json_to_sheet(data);
+        ws = XLSX.utils.json_to_sheet(print);
     
         XLSX.utils.book_append_sheet(wb, ws, 'Roster');
     
