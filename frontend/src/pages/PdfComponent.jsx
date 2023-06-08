3a9970de338b1deb9d503280dc3183391083e9ce
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-08 20:43:12 +0100

    Update PdfComponent.jsx

--- frontend/src/pages/PdfComponent.jsx
+++ frontend/src/pages/PdfComponent.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Document, Page, Text, PDFViewer } from 'react-pdf';
+import React, {useState, useEffect} from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf'
 
-const PdfComponent = () => {
-  const renderPDF = () => (
-    <Document>
-      <Page>
-          <Text>Hello, World!</Text>
-      </Page>
-    </Document>
-  );
-
-  return (
-    <PDFViewer width="100%" height="500px">
-      {renderPDF()}
-    </PDFViewer>
-  );
-};
-
-export default PdfComponent;
+export default function PdfComponent() {
+  
+  const [loading, setLoading] = useState(false);
+  
+  const download = () => {
+    const capture = document.querySelector('hello');
+    setLoading(true);
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL('img/png');
+      const doc = new jsPDF('p', 'mm', 'a4');
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+      setLoading(false);
+      doc.save('save.pdf')
+     
+    })
+    
+}v
+  
+  return(
+    <>
+      <h3 className='hello'>Hello, world</h3>
+      <button 
+      onClick={download}
+      disabled={loading}>{loading ? 'Loading' : 'Submit'}</button>
+    </>
+  )
+}
