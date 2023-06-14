16e0f22043fa1bbb6d8da8be1c22866a2631a5f2
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-14 03:48:52 +0100

    Update ClientForm.jsx

--- frontend/src/pages/ClientForm.jsx
+++ frontend/src/pages/ClientForm.jsx
@@ -1529,7 +1529,7 @@ export default function ClientForm() {
                  <div className='flex align' style={{margin: '0'}}>
                    <label>
                      Date:
-                   <input type="date" value={signDate} onChange={(e) => setSignDate(e.target.value)} style={{ width: '10.5rem'}} />
+                   <input type="date" required value={signDate} onChange={(e) => setSignDate(e.target.value)} style={{ width: '10.5rem'}} />
                    </label>
                  </div>
 
@@ -1611,7 +1611,7 @@ export default function ClientForm() {
            <div className='flex align block1x'>
               <label>
                 Date:
-                <input type="date" value={offSignDate} onChange={(e) => setOffSignDate(e.target.value)} style={{ width: '10.5rem'}}/>
+                <input type="date" required value={offSignDate} onChange={(e) => setOffSignDate(e.target.value)} style={{ width: '10.5rem'}}/>
               </label>
            </div>
 
