325eee816b2b1f900b455f67d5612a1f5522e83f
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-15 20:36:10 +0100

    Update SignForm.jsx

--- frontend/src/pages/SignForm.jsx
+++ frontend/src/pages/SignForm.jsx
@@ -1,8 +1,33 @@
 import { useEffect, useState } from 'react';
+import SignatureCanvas from 'react-signature-canvas';
+
 export default function SignForm() {
+  const [sign, setSign] = useState();
+  const [signDate, setSignDate] = useState('')
+  const [image, setImage] = useState();
+  
+  const handleClear = () => {
+        sign.clear();
+        setImage('');
+        setSignature('');
+    }
+
+    const handleSave = () => {
+        const saved = sign.getTrimmedCanvas().toDataURL('image/png');
+
+        if(saved){
+            setImage(saved);
+            sign.clear();
+
+            setSignature(saved);
+        }
+
+    }   
+  
   return(
     <div>
     
+
     </div>
   )
 }
