7e7120c6b732fb424e6b2154c5dfafea194e840a
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-15 14:55:27 +0100

    Update ClientUser.jsx

--- frontend/src/pages/ClientUser.jsx
+++ frontend/src/pages/ClientUser.jsx
@@ -3,7 +3,7 @@ import ClientForm from './ClientUserForm'
 export default function ClientUser() {
     const user = JSON.parse(localStorage.getItem('user'));
 
-    if(user.role != 'Branch User'){
+    if(user.role != 'Client'){
         return <h2>Error</h2>
     }
 
