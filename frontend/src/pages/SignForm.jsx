08a00d21af1ace4318fbd44f0d9a617278783d6c
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-15 21:33:46 +0100

    Update SignForm.jsx

--- frontend/src/pages/SignForm.jsx
+++ frontend/src/pages/SignForm.jsx
@@ -74,21 +74,26 @@ export default function SignForm() {
   
   return(
     <div>
-    <label>Signature</label>
-       <div style={{border: '2px solid black', width: '12rem', height: '96px'}} className='sign_pad'>    
-          <SignatureCanvas 
-            ref={data => setSign(data)}
-            canvasProps={{width: 287, height: 92, className: 'sigCanvas'}}
-           />
+    
+      <div className='signform'>
+        <label>Signature</label>
+        <div style={{border: '2px solid black', width: '12rem', height: '96px'}} className='sign_pad'>    
+            <SignatureCanvas 
+              ref={data => setSign(data)}
+              canvasProps={{width: 287, height: 92, className: 'sigCanvas'}}
+             />
 
-          { image && <div className='sign_preview'>
-              <img src={image} alt='signature' />
-           </div> }
+            { image && <div className='sign_preview'>
+                <img src={image} alt='signature' />
+             </div> }
+          </div>
+           
+          <div className='sign_buttons'>
+            <button className='button' type='button' onClick={handleSave}>Save</button>
+            <button className='button' type='button' onClick={handleClear}>Clear</button>
+          </div>
+
         </div>
-             <div className='sign_buttons'>
-               <button className='button' type='button' onClick={handleSave}>Save</button>
-               <button className='button' type='button' onClick={handleClear}>Clear</button>
-              </div>
 
     </div>
   )
