8e6be6d5ca30f2e5906ea612a33229cc937ac0bf
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-06-15 13:15:31 +0100

    Update CreateClient.jsx

--- frontend/src/pages/CreateClient.jsx
+++ frontend/src/pages/CreateClient.jsx
@@ -99,7 +99,9 @@ export default function CreateClient() {
                 email,
                 role,
                 password,
-                branch
+                branch,
+                userCreator
+            
             } 
             
 
