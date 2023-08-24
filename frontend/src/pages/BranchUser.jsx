1e586e8ed47d74c3c7677a23d34c59ef46632da0
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-08-24 15:17:47 +0100

    Update BranchUser.jsx

--- frontend/src/pages/BranchUser.jsx
+++ frontend/src/pages/BranchUser.jsx
@@ -106,7 +106,7 @@ export default function BranchUser() {
 
                 <h3 className='block branch_subhead'>Submitted Client Forms</h3>
                 <div className='flex_button'>                    
-                    <Link to={`/client/${user.branch}`}><button className='button radius bottom' >Add Client</button></Link>
+                    {/* <Link to={`/client/${user.branch}`}><button className='button radius bottom' >Add Client</button></Link> */}
                     <Link to={`/roster/${user.branch}`}><button className='button radius bottom'>View Roster</button></Link>
                 </div>
 
