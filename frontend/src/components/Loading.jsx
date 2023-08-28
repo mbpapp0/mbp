933957d66a82a2f8c2ef4f8646f9d7a3b96b75e7
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-08-28 09:58:26 +0100

    Create Loading.jsx

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return(
+    <div className="loading-container">
+      <span className="loading-text">Loading</span>
+      <div className="dot-container">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    </div>
+  )
+}
