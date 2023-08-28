b96a140c5589f48c3ce548525a8021daf68d9cea
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-08-28 06:21:22 +0100

    Update StartNewForm.jsx

--- frontend/src/pages/StartNewForm.jsx
+++ frontend/src/pages/StartNewForm.jsx
@@ -1,1 +1,16 @@
+import { useState } from 'react';
 
+export default function StartNewForm() {
+  const [newForm, setNewForm] = useState(false);
+
+  const restartForm = () => {
+    setNewForm(true);
+  }
+  
+  return(
+    <div>
+      {newForm && <h3>It worked</h3> }
+      <button onClick={restartForm} className="button radius">Start New Form</button>
+    </div>
+  )
+}
