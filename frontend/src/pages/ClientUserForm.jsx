54a85b9d103fe373f7950aea6b7e64920d2b6acb
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-08-28 07:20:31 +0100

    Update ClientUserForm.jsx

--- frontend/src/pages/ClientUserForm.jsx
+++ frontend/src/pages/ClientUserForm.jsx
@@ -1079,13 +1079,16 @@ export default function ClientUserForm() {
     } 
     
     if(formExists){
-      return(<h2 style={{marginTop: '7rem', textAlign: 'center'}}>{ formExists == 'Approved' ? 'IEG Form Approved' : 'IEG Form Pending Approval'}</h2>)
+      return(
+          <div style={{marginTop: '7rem', textAlign: 'center'}}>
+            <StartNewForm formExists={formExists} setFormExists={setFormExists} /> 
+          </div>
+      )
     }
 
     
     return(
         <>
-        <StartNewForm />
         <h2 className='heading'>{branchName} Branch.</h2>
         <h2 className='heading'>CACFP Meal Benefit Income Eligibility Statement</h2>
         {error && 
