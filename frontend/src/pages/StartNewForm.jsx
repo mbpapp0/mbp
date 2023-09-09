34593192a0881c68c39dd25929d20a75af710229
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-09-09 02:38:51 +0100

    Update StartNewForm.jsx

--- frontend/src/pages/StartNewForm.jsx
+++ frontend/src/pages/StartNewForm.jsx
@@ -8,6 +8,7 @@ export default function StartNewForm(props) {
   
   return(
     <div>
+      <h2 className='heading'>{props.branchName} Branch.</h2>
       {props.submitted && <h2 style={{marginBottom: '1.5rem'}}>Income Eligibility Form Submitted</h2>}
       <button onClick={restartForm} className="button radius">Start a New Income Eligibility Application</button>
     </div>
