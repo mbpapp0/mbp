40a05e74f71aa57dbe68ec8ef4fd12bd2cd28f08
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-09-30 20:21:51 +0100

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -78,7 +78,7 @@ export default function Header() {
       window.removeEventListener('mousemove', handleUserActivity);
       window.removeEventListener('keydown', handleUserActivity);
     };
-  }, []);
+  }, [lastLoginTime]);
 
 
     return (
