0857f12e4071895b1ea1e897f9ba9af577280d1d
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-10-15 18:36:46 +0100

    Update IEG.jsx

--- frontend/src/IEG.jsx
+++ frontend/src/IEG.jsx
@@ -52,7 +52,7 @@ export default function IEG() {
 
     return(
     <div>
-      <h3 clasName="center">{branchName}.</h3>
+      <h3 clasName="center">{branchName} Branch.</h3>
       <h3 className="center">CACFP Meal Benefit Income Eligibility Statement</h3>
   <form>
     <table>
