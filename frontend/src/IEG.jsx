14d9c653f7e7c35bcab29a32d62ed0a2c37b644b
mbpapp0 <134592473+mbpapp0@users.noreply.github.com>
2023-10-15 18:34:17 +0100

    Update IEG.jsx

--- frontend/src/IEG.jsx
+++ frontend/src/IEG.jsx
@@ -48,3 +48,95 @@ export default function IEG() {
     if(user.role != 'Branch User'){
         window.location.assign('/');
     }
+
+    return(
+    <div>
+      <h3 clasName="center">{branchName}.</h3>
+      <h3 className="center">CACFP Meal Benefit Income Eligibility Statement</h3>
+  <form>
+    <table>
+      <tbody>
+        
+        <tr>
+           <td colSpan={7} className="part_header">
+    PART I: Child(ren) or Adult enrolled to receive day care
+          </td>
+         </tr>
+         <tr>
+           <td rowSpan={2} className="name">
+    <strong>
+      <p>Name: (Last, First and Middle Initial)</p>
+    </strong>
+  </td>
+  <td rowSpan={2} className="case_number">
+    SNAP, TANF, or FDPIR case number, or Client ID number for children only. All the above, or SSI or Medicaid case number for Adults. <strong>Note:</strong> Do not use EBT numbers. Write case number and proceed to Part III.
+  </td>
+  <td colSpan={5}>
+    <strong>Children in Head Start, foster care and children who meet the definition of migrant, runaway, or homeless are eligible for free meals. Check () all that apply. </strong>
+    <span id="red">(See definitions in FAQs)</span>
+  </td>
+</tr>
+
+<tr>
+  <td className="center"><span id="highlighted">Head Start</span></td>
+  <td className="center"><span id="highlighted">Foster Child</span></td>
+  <td className="center"><span id="highlighted">Migrant</span></td>
+  <td className="center"><span id="highlighted">Runaway</span></td>
+  <td className="center"><span id="highlighted">Homeless</span></td>
+</tr>
+
+<tr className="check_td">
+        <td><input type="text" value={data.childOneName} /></td>
+        <td><input type="text" value={data.childOneID} /></td>
+        <td><input type="checkbox" checked={data.childOneHeadStart}/></td>
+        <td><input type="checkbox" checked={data.childOneForsterChild}/></td>
+         <td><input type="checkbox" checked={data.childOneMigrant}/></td>
+        <td><input type="checkbox" checked={data.childOneRunaway}/></td>
+        <td><input type="checkbox" checked={data.childOneHomeless}/></td>
+       
+      </tr>
+      <tr className="check_td">
+        <td><input type="text" value={data.childTwoName} /></td>
+<td><input type="text" value={data.childTwoID} /></td>
+<td><input type="checkbox" checked={data.childTwoHeadStart}/></td>
+<td><input type="checkbox" checked={data.childTwoForsterChild}/></td>
+<td><input type="checkbox" checked={data.childTwoMigrant}/></td>
+<td><input type="checkbox" checked={data.childTwoRunaway}/></td>
+<td><input type="checkbox" checked={data.childTwoHomeless}/></td>
+      </tr>
+      <tr className="check_td">
+         <td><input type="text" value={data.childThreeName} /></td>
+<td><input type="text" value={data.childThreeID} /></td>
+<td><input type="checkbox" checked={data.childThreeHeadStart}/></td>
+<td><input type="checkbox" checked={data.childThreeForsterChild}/></td>
+<td><input type="checkbox" checked={data.childThreeMigrant}/></td>
+<td><input type="checkbox" checked={data.childThreeRunaway}/></td>
+<td><input type="checkbox" checked={data.childThreeHomeless}/></td>
+      </tr>
+      <tr className="check_td">
+        <td><input type="text" value={data.childFourName} /></td>
+<td><input type="text" value={data.childFourID} /></td>
+<td><input type="checkbox" checked={data.childFourHeadStart}/></td>
+<td><input type="checkbox" checked={data.childFourForsterChild}/></td>
+<td><input type="checkbox" checked={data.childFourMigrant}/></td>
+<td><input type="checkbox" checked={data.childFourRunaway}/></td>
+<td><input type="checkbox" checked={data.childFourHomeless}/></td>
+      </tr>
+      <tr className="check_td">
+       <td><input type="text" value={data.childFiveName} /></td>
+<td><input type="text" value={data.childFiveID} /></td>
+<td><input type="checkbox" checked={data.childFiveHeadStart}/></td>
+<td><input type="checkbox" checked={data.childFiveForsterChild}/></td>
+<td><input type="checkbox" checked={data.childFiveMigrant}/></td>
+<td><input type="checkbox" checked={data.childFiveRunaway}/></td>
+<td><input type="checkbox" checked={data.childFiveHomeless}/></td>
+      </tr>
+
+      </tbody>
+    </table>
+  </form>
+    </div>
+  )
+  
+}
+
